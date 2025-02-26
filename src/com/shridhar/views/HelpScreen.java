ce66eca11bed9d98d22c738366eeb3a815701e9d
JaumeAL <jsalas2@nigul.cide.es>
2025-02-26 18:36:24 +0100

    z

--- src/com/shridhar/views/HelpScreen.java
+++ src/com/shridhar/views/HelpScreen.java
@@ -33,6 +33,7 @@ public class HelpScreen extends JFrame {
 
 	
 	public HelpScreen() {
+		
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 501, 447);
 		contentPane = new JPanel();
